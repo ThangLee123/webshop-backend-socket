58bf137cca82dc85e80407073d87af56168a6d47
Thang <thangle220909@gmail.com>
2022-12-25 17:14:36 +0700

    initial commit socket

---
+++ index.js
@@ -0,0 +1,99 @@
+const port = process.env.PORT || 8900;
+console.log("port socket", port);
+const frontendURL = process.env.FRONTEND_URL || "http://localhost:3000";
+const io = require("socket.io")(port, {
+  cors: {
+    origin: frontendURL,
+  },
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+const removeUserById = (userId) => {
+  users = users.filter((user) => user.userId !== userId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+io.on("connection", (socket) => {
+  // When connect
+  console.log("a user connected.");
+  //   io.emit("welcome", "hello this is socket server!");
+
+  // remove user when log out
+  socket.on("removeUser", (userId) => {
+    removeUserById(userId);
+    console.log("online users", users);
+    io.emit("getUsers", users);
+  });
+
+  // take userId and socketId from user
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    console.log("online users", users);
+    io.emit("getUsers", users);
+  });
+
+  // send and get message
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    if (user) {
+      io.to(user.socketId).emit("getMessage", {
+        senderId,
+        text,
+      });
+    }
+  });
+
+  // send and get new Notification rollback order
+  socket.on("sendNotify", () => {
+    io.emit("getNotify", {});
+  });
+
+  // send and get admin watch Notification rollback order
+  // socket.on("sendAdminWatchNotifyRollback", () => {
+  //   io.emit("getAdminWatchNotifyRollback", {});
+  // });
+
+  // send and get new Notification new order create
+  socket.on("sendNotifyNewOrder", () => {
+    io.emit("getNotifyNewOrder", {});
+  });
+
+  // send and get new product comment
+  socket.on("sendProductComment", () => {
+    io.emit("getProductComment", {});
+  });
+
+  // send and get success pay order
+  socket.on("sendSuccessPay", () => {
+    io.emit("getSuccessPay", {});
+  });
+
+  // send and get success deliver
+  socket.on("sendSuccessDeliver", () => {
+    io.emit("getSuccessDeliver", {});
+  });
+
+  // send and get success deliver
+  socket.on("sendNotifyHandleRollback", () => {
+    io.emit("getNotifyHandleRollback", {});
+  });
+
+  // When disconnect
+  socket.on("disconnect", () => {
+    console.log("someone has left");
+    removeUser(socket.id);
+    console.log("online users", users);
+    io.emit("getUsers", users);
+  });
+});
